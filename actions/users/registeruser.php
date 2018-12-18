d8bc80f619afa64dee725c1ea2bb5cabb34b982a
joaopedro2804 <45626640+joaopedro2804@users.noreply.github.com>
2018-12-18 17:35:08 +0000

    Add files via upload

--- actions/users/registeruser.php
+++ actions/users/registeruser.php
@@ -2,25 +2,27 @@
   include_once('../../config/init.php');
   include_once($BASE_DIR .'database/users.php');  
 
-  
-  $username = $_POST['user'];
-  $mail     = $_POST['mail'];
-  $pass     = $_POST['pass'];
-  $passw    = $_POST['passw'];
-  $name     = $_POST['name'];
-  $age      = $_POST['age'];
-  $gender   = $_POST['gender'];
-  $address  = $_POST['address'];
-  $country  = $_POST['country'];
-  $city     = $_POST['city'];
 
-  $username = strip_tags($_POST['username']);
-  $mail     = strip_tags($_POST['mail']);
+  $username       = $_POST['username'];
+  $password       = $_POST['password'];
+  $repeatpassword = $_POST['repeatpassword'];
+  $name           = $_POST['name'];
+  $email          = $_POST['email'];
+  $age            = $_POST['age'];
+  $gender         = $_POST['gender'];
+  $address        = $_POST['address'];
+  $country        = $_POST['country'];
+  $city           = $_POST['city'];
+
   $name     = strip_tags($_POST['name']);
-  $adress   = strip_tags($_POST['adress']);
+  $username = strip_tags($_POST['username']);
+  $email    = strip_tags($_POST['email']);
+  $age      = strip_tags($_POST['age']);
+  $address  = strip_tags($_POST['address']);
   $country  = strip_tags($_POST['country']);
   $city     = strip_tags($_POST['city']);
 
-  createUser($username, $mail, $pass, $passw, $name, $age, $gender, $adress, $country, $city);
+
+  createUser($username, $password, $name, $email, $gender, $age, $country, $city, $address);
   header("Location: $BASE_URL");
 ?>
